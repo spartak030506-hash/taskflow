78cc758a0c8cb8d676d2087d91e87507c2822fd4
Danil <spartak030506@example.com>
2026-02-11 23:09:16 +0700

    ci

--- config/settings/test.py
+++ config/settings/test.py
@@ -4,13 +4,6 @@ DEBUG = False
 
 PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
-}
-
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
