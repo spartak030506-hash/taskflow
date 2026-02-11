434d2fca8fbb925471437385f8711483fe4d69a6
Danil <spartak030506@example.com>
2026-02-11 21:52:47 +0700

    улучшеннения докер  файла и докер компос, добавления dockerignore

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,12 +6,16 @@ echo "Waiting for PostgreSQL..."
 while ! nc -z $DB_HOST $DB_PORT; do
     sleep 0.5
 done
+echo "PostgreSQL port is open"
+sleep 1
 echo "PostgreSQL is ready"
 
 echo "Waiting for Redis..."
 while ! nc -z $REDIS_HOST $REDIS_PORT; do
     sleep 0.5
 done
+echo "Redis port is open"
+sleep 1
 echo "Redis is ready"
 
 exec "$@"
